b86fffcd9389aeeaa9cda2b8dc706492efcdf686
Davit Grdzelishvili <166071024+davita101@users.noreply.github.com>
2025-03-24 02:43:15 +0400

    start: portofolio website

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+
+export default function App() {
+    return (
+        <main>
+           <Navbar />
+            <Hero />
+        </main>
+    )
+}
---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+    {
+        id: 1,
+        title: "Home",
+        href: "#Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        title: "About",
+        href: "#About",
+        url: "/about",
+    },
+    {
+        id: 3,
+        title: "Services",
+        href: "#Services",
+        url: "/services",
+    },
+    {
+        id: 4,
+        title: "Contact",
+        href: "#Services",
+        url: "/contact",
+    },
+];
